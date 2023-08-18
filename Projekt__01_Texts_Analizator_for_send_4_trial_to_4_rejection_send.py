08ed339f3ec6e9f08a48574406fe9a56308c3ee9
tarabanprojects <137524761+tarabanprojects@users.noreply.github.com>
2023-08-18 11:39:57 +0200

    Add files via upload

---
+++ Projekt__01_Texts_Analizator_for_send_4_trial_to_4_rejection_send.py
@@ -0,0 +1,66 @@
+"""
+projekt_1.py: první projekt do  Engeto Online  Python Akademie
+  
+author: Igor Taraban
+e-mail: taraban@centrum.cz
+discord: igort2408     - PROSIM  odpoved  poslat na e-mail ! Děkuji moc ! 
+                            DISCORD ... moc  nefunguji ! Děkuji moc ! 
+"""
+
+TEXTS = ['''
+Situated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley. ''',
+'''At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.'''
+]
+
+pir_us={input("Enter your name, please: "):input("Enter your password, please:  ")}
+pir_us={"bob":"123","ann":"pass123", "mike":"password123", "liz":"pass123"}
+cont = all((pair_user.get(key) ==  v for key, ff in pair_user.items()))
+
+if control == False :
+    print("$ python project1.py")
+    
+else:
+    print(" xg tud opizt " )
+             else:
+                ght bbg
+                print(f"There are {BIG} uppercase words.")
+                  zh kiz
+        for value_9 in value: 
+            
+            if value_10 is True :
+             
+              else:
+                bf jzio
+        print(f"There are {numer} numeric strings.") 
+ 
+        print("------------------------------------------")
+    
+        print("     1|" , "*" * number_1, " "*(16-number_1), "|",number_1, 
+    else:
+        print("bbuka_hru")
+        print(f"btu_hti:",*paghzredcvn,())
+        print(f"password:",*pbbfgju    jjj()) 
+        print("entered text number is wrong, terminating  the  program ..." )
